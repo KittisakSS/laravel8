05c1151b823c3124f3b408f093f26a4b9543943c
KittisakSS <kittisaksonsing@gmail.com>
2024-01-25 19:12:39 +0700

    week9

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -230,6 +232,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
+
 
     ],
 
